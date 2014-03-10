2367544bde057c728ac84c52c78c1fa9d9b234f7
proflayton <brandon0layton@yahoo.com>
2014-03-10 15:58:55 -0700

    AVI implementation added
    The beginning stages of AVI implementation was added

--- AVI.py
+++ AVI.py
@@ -1,7 +1,36 @@
 '''
 Handle .AVI files
 '''
-import RIFF
+'''
+Layout:
+RIFF AVI //mandatory
+{ RIFF AVIX } //only for Open-DML files
+'''
 
-class AVI(RIFF):
+class Chunk:
+	#describes type of chunk/list
+	#2 hexadecimal digits specifying the stream number
+	#2 letters specifying the data type
+	#dc = video
+	#wb = audio
+	#tx = text
+	fourCC = None
+
+	#size of the data
+	dwSize = None #includes first byte after
+
+	#data
+	dwData = None
+
+#fourCC = 'AVI ' is a "RIFF-AVI-List"
+#fourCC = 'AVIX' is a "RIFF_AVIX-List"
+class List:
+	dwList = None
+	dwSize = None #includes 4 bytes taken by fourCC
+	fourCC = None
+	dwData = None
+
+class AVI:
 	
+	def __init__(self):
+		print("Initialize AVI")
